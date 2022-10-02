8f8b202e5591e242412b38d7c33ea3efdae677fb
alessandro <campoloalex@gmail.com>
2022-10-02 13:17:45 +0200

    Removed shell dependency, each command is called directly Replaced as much external commands with python functions Changed behaviour: do not abort if any of the partition is not mounted, continue unmounting even if one of the unmount fails

--- autodisk.py
+++ autodisk.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
+from grp import getgrnam
+from json import loads
+from os import chown, geteuid, kill, remove, rmdir
+from pathlib import Path
+from pwd import getpwnam
+from re import search, sub
+from shutil import rmtree
+from subprocess import CalledProcessError, check_output, run
+from sys import stderr
+from time import sleep
+
 from inotify.adapters import Inotify
 from inotify.calls import InotifyError
-from inotify.constants import IN_CREATE, IN_DELETE, IN_DELETE_SELF
-from yaml import safe_load, dump
-from time import sleep
-from json import loads
-from re import sub, search
-from subprocess import check_output, run, CalledProcessError
-from pathlib import Path
-from os import chown, geteuid
-from sys import stderr
-from pwd import getpwnam
-from grp import getgrnam
+from inotify.constants import IN_CREATE, IN_DELETE, IN_DELETE_SELF, IN_MOVED_TO
+from yaml import dump, safe_load
 
 if geteuid() != 0:
     exit("You need to have root privileges to run this script.\nPlease try again, this time using 'sudo'. Exiting.")
@@ -28,7 +30,7 @@ if not CONFIG_FILE.exists():
     CONFIG_FILE.parent.mkdir(parents=True, exist_ok=True)
     with open(str(CONFIG_FILE), 'w', encoding='utf8') as configfile:
         dump(CONFIG, configfile, default_flow_style=False, allow_unicode=True)
-    print('Configuration file ' + str(CONFIG_FILE) + ' not found, created.', file=stderr)
+    print(f'Configuration file {CONFIG_FILE} not found, created.', file=stderr)
     exit()
 
 
@@ -66,8 +68,7 @@ def sanitize(input_str) -> str:
 
 
 def load_disk(block_dev):
-    disk_json = loads(check_output("lsblk -Jio KNAME,MODEL,LABEL,PARTLABEL,SIZE " + _DISK_PATH_ROOT + block_dev,
-                                   shell=True).decode("utf-8").strip())
+    disk_json = loads(check_output(['lsblk', '-Jio', 'KNAME,MODEL,LABEL,PARTLABEL,SIZE', _DISK_PATH_ROOT + block_dev]).decode("utf-8").strip())
     for blk in disk_json["blockdevices"]:
         if not search("(sd[a-z][0-9]*$)", blk['kname']):
             # First check if all volumes are simple devices controlled by
@@ -77,6 +78,7 @@ def load_disk(block_dev):
             # Chances are if you have one of these volumes you might want to manage them manually
             break
     else:
+        mounted = 0
         for blk in disk_json["blockdevices"]:
             if block_dev not in NAMES:
                 NAMES[block_dev] = {}
@@ -91,7 +93,7 @@ def load_disk(block_dev):
                 # else:        use partition label (used by linux)
                 # last resort: device name                
 
-                mnt_dir = Path("{}/{}/{}".format(_MOUNT_PATH_ROOT, NAMES[block_dev]["name"], disk_part))
+                mnt_dir = Path(f'{_MOUNT_PATH_ROOT}/{NAMES[block_dev]["name"]}/{disk_part}')
                 mnt_dir.mkdir(mode=CONFIG['mount_path_perms'], parents=True, exist_ok=True)
                 # since this program is aimed at short, not permanent,
                 # drive mounts we mount drives with `sync` option,
@@ -99,9 +101,13 @@ def load_disk(block_dev):
                 # neither we care too much about write cycles,
                 # we need to be sure to write instantly for fast unmounting
                 # and to be protected from unsecure ejections, power losses, etc...
-                run("mount -o sync,uid={},gid={} /dev/{} {}".format(
-                    CONFIG['mount_path_uid'], CONFIG['mount_path_gid'], blk['kname'], mnt_dir.resolve()), shell=True)
-                create_umount_file(block_dev)
+                run(['mount', '-o', f'sync,uid={CONFIG["mount_path_uid"]},gid={CONFIG["mount_path_gid"]}', f'/dev/{blk["kname"]}', mnt_dir.resolve()])
+                if str(mnt_dir.resolve()) not in check_output("mount").decode('utf-8'):
+                    rmdir(mnt_dir.resolve()) #if mount failed remove useless folder
+                else:
+                    mounted+=1
+        if mounted:
+            create_umount_file(block_dev)
 
 
 def create_umount_file(block_dev):
@@ -118,35 +124,40 @@ def create_umount_file(block_dev):
     umnt_file = Path(str(_UMOUNT_FILES_FOLDER.resolve()) + "/DELETE_THIS_FILE_TO_UNMOUNT_" + NAMES[block_dev]['name'])
     umnt_file.touch(mode=0o777)
     NAMES['umnt_files'][str(umnt_file.resolve())] = block_dev
-    print(NAMES)
-    print('Adding watch for unmount file {}'.format(umnt_file.resolve()))
+    print(f'Adding watch for unmount file {umnt_file.resolve()}')
     watcher.add_watch(str(umnt_file.resolve()), mask=IN_DELETE | IN_DELETE_SELF)
 
 
 def unmount(block_dev) -> bool:
     block_name = NAMES[block_dev]["name"]
+    errs = 0
     for part_num, part_name in NAMES[block_dev].items():
-        if "part" in part_num:
+        mnt_dir = f'{_MOUNT_PATH_ROOT}/{block_name}/{part_name}'
+        if "part" in part_num and mnt_dir in check_output("mount").decode('utf-8'):
             try:
-                print('Unmounting {}/{}/{}'.format(_MOUNT_PATH_ROOT, block_name, part_name))
+                print(f'Unmounting {mnt_dir}')
                 # we need to manually kill (sending SIGTERM)
                 # all smbd processes that keep mountpoint locked
                 # afaik there is no way to avoid this
                 #
                 # if other programs behave the same way we'll
                 # need to write a proper routine to kill them all
-                run("lsof -atc smbd {}/{}/{} | xargs -r kill".format(_MOUNT_PATH_ROOT, block_name, part_name),
-                    shell=True)
-                check_output("umount {}/{}/{}".format(_MOUNT_PATH_ROOT, block_name, part_name), shell=True)\
-                    .decode('utf-8').strip()
+                #run(f'lsof -atc smbd {mnt_dir} | xargs -r kill', shell=True)
+                for pid in check_output(['lsof', '-atc', 'smbd', mnt_dir]).decode('utf-8').split():
+                    kill(int(pid), 9)
+                check_output(['umount', mnt_dir]).decode('utf-8').strip()
             except CalledProcessError:
-                print('There was an error unmounting {}, skipping...'.format(part_name))
-                run('beep -f 800 -l 750 -r 2 -d 750', shell=True)
-                break
-    else:
+                errs += 1
+                print(f'There was an error unmounting {part_name}, skipping...')
+                run(['beep', '-f', '800', '-l', '750', '-r', '2', '-d', '750'])
+    if not errs:
         NAMES['umnt_files'].pop(str(Path(str(_UMOUNT_FILES_FOLDER.resolve()) + "/DELETE_THIS_FILE_TO_UNMOUNT_" +
                                          NAMES[block_dev]['name']).resolve()), None)
-        run("rm -r {}/{} && beep".format(_MOUNT_PATH_ROOT, block_name), shell=True)
+        try:
+            rmtree(f'{_MOUNT_PATH_ROOT}/{block_name}')
+            run('beep')
+        except Exception:
+            pass
         return True
     return False
 
@@ -159,10 +170,10 @@ print("Devices:")
 print(NAMES)
 print("Adding watches")
 
-watcher.add_watch(_DISK_PATH_ROOT, mask=IN_CREATE | IN_DELETE)
+watcher.add_watch(_DISK_PATH_ROOT, mask=IN_CREATE | IN_MOVED_TO | IN_DELETE)
 for (_, type_names, path, filename) in watcher.event_gen(yield_nones=False):
     if filename in CONFIG['devices']:
-        if 'IN_CREATE' in type_names:
+        if 'IN_CREATE' in type_names or 'IN_MOVED_TO' in type_names:
             print("Inserted " + filename)
             sleep(2)
             load_disk(filename)
@@ -172,13 +183,13 @@ for (_, type_names, path, filename) in watcher.event_gen(yield_nones=False):
             for key, val in NAMES['umnt_files'].items():
                 if val == filename:
                     watcher.remove_watch(key)
-                    run('rm {}'.format(key), shell=True)
+                    remove(key)
                     unmount(NAMES['umnt_files'].pop(key, None))
                     break
             NAMES.pop(filename, None)
     elif path in NAMES['umnt_files']:
         if 'IN_DELETE' in type_names or 'IN_DELETE_SELF' in type_names:
-            print('Deleted {}, unmounting related filesystems'.format(path))
+            print(f'Deleted {path}, unmounting related filesystems')
             # remove from watch list even if it is not being watched anymore
             # not doing so results in "Path already being watched" when drive is remounted,
             # while the program is actually watching the old deleted file and not the new one with same name
